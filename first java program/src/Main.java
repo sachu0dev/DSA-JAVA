5cb462d30db9f582b407ed322fca8d097c5ebe3f
sachu0dev <sushil.dev.in@gmail.com>
2024-03-22 22:49:14 +0530

    jj

---
+++ first java program/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public  class Main {
+    Scanner input = new Scanner(System.in);
+    //        System.out.println(input.nextLine());
+//        primitive datatypes
+//        int rollno = 64;
+//        char letter = "s";
+//        float marks = 89.74f;
+//        double largeDecimalNum = 93474983274.374;
+//        long largeInteger = 73490872389L;
+//        boolean check = false;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("please give me input");
+        int rollno = input.nextInt();
+        System.out.println("your roll number is : " + rollno);
+    }
+}
